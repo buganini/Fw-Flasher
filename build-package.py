b7e802b3601c358ea53e798627ce4bf95d36cd53
Buganini Chiu <buganini@b612.tw>
2025-03-15 22:25:58 +0800

    build-package: disable json bundling

--- build-package.py
+++ build-package.py
@@ -23,11 +23,11 @@ if platform.system()=="Darwin":
 # else:
 #     pyinstaller_args.extend(["-i", 'resources/icon.ico'])
 
-if len(sys.argv) > 1 and sys.argv[1].endswith(".json"):
-    shutil.copy(sys.argv[1], "build/manifest.json")
-    pyinstaller_args.extend(["--add-data", "build/manifest.json:."])
-elif os.path.exists("manifest.json"):
-    pyinstaller_args.extend(["--add-data", "manifest.json:."])
+# if len(sys.argv) > 1 and sys.argv[1].endswith(".json"):
+#     shutil.copy(sys.argv[1], "build/manifest.json")
+#     pyinstaller_args.extend(["--add-data", "build/manifest.json:."])
+# elif os.path.exists("manifest.json"):
+#     pyinstaller_args.extend(["--add-data", "manifest.json:."])
 
 print(pyinstaller_args)
 
