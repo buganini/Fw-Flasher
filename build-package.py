75c4b76b5b2bbc1223d80a2890578e6bf8bcde7d
Buganini Chiu <buganini@b612.tw>
2025-03-15 22:13:17 +0800

    build-package.py: allow specifying bundled json

--- build-package.py
+++ build-package.py
@@ -2,9 +2,10 @@ import PyInstaller.__main__
 import platform
 import subprocess
 import os
+import sys
 import itertools
 import glob
-
+import shutil
 
 create_dmg = False
 codesign_identity = None
@@ -22,7 +23,10 @@ if platform.system()=="Darwin":
 # else:
 #     pyinstaller_args.extend(["-i", 'resources/icon.ico'])
 
-if os.path.exists("manifest.json"):
+if len(sys.argv) > 1 and sys.argv[1].endswith(".json"):
+    shutil.copy(sys.argv[1], "build/manifest.json")
+    pyinstaller_args.extend(["--add-data", "build/manifest.json:."])
+elif os.path.exists("manifest.json"):
     pyinstaller_args.extend(["--add-data", "manifest.json:."])
 
 print(pyinstaller_args)
