febaf78da9696bde199a15d3fb0d2666086a658b
buganini <buganini@b612.tw>
2025-03-18 21:16:37 +0800

    fix finding arm_none_eabi_gdb on windows

--- FwFlasher.py
+++ FwFlasher.py
@@ -69,7 +69,7 @@ def find_arm_none_eabi_gdb():
             arm_none_eabi_gdb = None
     except Exception:
         base_path = os.path.dirname(sys.argv[0])
-        arm_none_eabi_gdb = glob.glob(os.path.join(base_path, "gcc-arm-none-eabi-*/bin/arm-none-eabi-gdb"))
+        arm_none_eabi_gdb = glob.glob(os.path.join(base_path, "gcc-arm-none-eabi-*/bin/arm-none-eabi-gdb*"))
         if arm_none_eabi_gdb:
             arm_none_eabi_gdb = arm_none_eabi_gdb[0]
         else:
