aa6c6077daee47e5ea22176b9fd6a420602c3617
Buganini Chiu <buganini@b612.tw>
2026-05-19 23:33:47 +0800

    add pyocd cli invoker

--- FwFlasher/__init__.py
+++ FwFlasher/__init__.py
@@ -15,10 +15,14 @@ def main(args=None):
         if args[0] == "esptool":
             esptool.main(args[1:])
             return
+        if args[0] == "pyocd":
+            sys.argv.pop(0)
+            from pyocd import __main__ as pyocd_main
+            pyocd_main.main()
+            return
         else:
             ui.loadFile(args[0])
     elif os.path.exists("manifest/manifest.json"):
         ui.loadFile("manifest/manifest.json")
 
     ui.run()
-
