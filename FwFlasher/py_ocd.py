eb7f15afb13ec6e02b8f01179ffad0ad818140be
Buganini Chiu <buganini@b612.tw>
2026-05-21 17:37:16 +0800

    reorganize pyocd code

--- FwFlasher/py_ocd.py
+++ FwFlasher/py_ocd.py
@@ -2,6 +2,56 @@ import os
 import re
 from .common import *
 from pyocd.core.helpers import ConnectHelper
+from pyocd.flash.file_programmer import FileProgrammer
+from pyocd.target.family.target_nRF91 import ModemUpdater
+
+def sequencer(port, target, frequency, seq):
+    kwargs = {
+        **({"unique_id": port} if port != "Auto" else {}),
+        "options": {
+            **({"frequency": frequency} if frequency else {}),
+        }
+    }
+    with ConnectHelper.session_with_chosen_probe(target_override=target, **kwargs) as session:
+        target = session.board.target
+
+        write_cmds_num = 0
+        seq2 = list(seq)
+        while seq2:
+            cmd = seq2.pop(0)
+            if cmd in ("load", "nrf91-update-modem-fw"):
+                seq2.pop(0)
+                write_cmds_num += 1
+
+        write_cmds_done = 0
+        seq = list(seq)
+        while seq:
+            cmd = seq.pop(0)
+            progress = int(write_cmds_done / write_cmds_num * 100)
+            print("+PROGRESS:", progress, flush=True)
+            if cmd == "erase":
+                print("Erasing flash...", flush=True)
+                target.mass_erase()
+                print("Flash erased", flush=True)
+            elif cmd == "load":
+                file = seq.pop(0)
+                print(f"Loading {file}...", flush=True)
+                def progress(progress):
+                    progress = int(write_cmds_done / write_cmds_num * 100) + (progress*100)/write_cmds_num
+                    print("+PROGRESS:", progress, flush=True)
+                programmer = FileProgrammer(session, progress=progress)
+                programmer.program(file)
+                write_cmds_done += 1
+            elif cmd == "nrf91-update-modem-fw":
+                file = seq.pop(0)
+                print(f"nrf91-update-modem-fw {file}...", flush=True)
+                def progress(progress):
+                    progress = int(write_cmds_done / write_cmds_num * 100) + (progress*100)/write_cmds_num
+                    print("+PROGRESS:", progress, flush=True)
+                update = ModemUpdater(session, progress=progress)
+                update.program_and_verify(file)
+                write_cmds_done += 1
+
 
 class PyOCDBackend(Backend):
     show_progress = True
